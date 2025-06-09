780d91b6ce8234cd91f0f3db1dabd1007364fe1e
NagarjunaNaidu141 <nagarjunanaidu141@gmail.com>
2025-06-09 08:09:44 +0530

    new SwiftUI

---
+++ SwiftUIPractice/SwiftUIPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIPracticeApp.swift
+//  SwiftUIPractice
+//
+//  Created by Nagarjuna Naidu on 09/06/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
